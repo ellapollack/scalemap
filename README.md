d69e15b4b4834dd61971c62d0214665c4b5d05b9
Maxwell Pollack <mail@maxis.cool>
2019-12-14 13:49:28 -0500

    Thread-safe `fromString`, updated documentation

--- README.md
+++ README.md
@@ -1,8 +1,76 @@
-## Currently under development
+# `ScaleMap`
+is a C++ class which converts note numbers to frequencies, based on a user-definable musical scale.
+
+Add `ScaleMap` to your project with
 
 ```cpp
-#include <scalemap.cpp>
-
-ScaleMap sm; // 12-tone equal temperament by default
-sm.midiToFreq(69); // 440.
+#include "scalemap.h"
 ```
+
+By default, it uses [A440 12-tone equal temperament](https://en.wikipedia.org/wiki/Equal_temperament).
+
+```cpp
+ScaleMap sm;
+
+for (int note=60; note<72; ++note)
+  cout << note << " : " << sm.noteToFreq(note) << "\n";
+```
+```console
+60 : 261.626
+61 : 277.183
+62 : 293.665
+63 : 311.127
+64 : 329.628
+65 : 349.228
+66 : 369.994
+67 : 391.995
+68 : 415.305
+69 : 440
+70 : 466.164
+71 : 493.883
+```
+To load a new scale, call `fromString(std::string scale)`, with `scale` in the following format:
+```
+baseNote : baseFreq
+freqRatio1
+freqRatio2
+freqRatio3
+.
+.
+.
+freqRatioOctave
+```
+
+- `baseNote` is an integer, and sets the note at which the scale begins.
+- `baseFreq` is a math expression, and sets the frequency of `baseNote`.
+- `freqRatioN` is a math expression, and sets the frequency ratio of the Nth scale degree.
+- The final `freqRatioN` is used as the octave.
+- All math expressions are parsed with [TinyExpr](https://codeplea.com/tinyexpr).
+- All non-newline whitespace is ignored.
+
+So for example, to load [A432 Pythagorean tuning](https://en.wikipedia.org/wiki/Pythagorean_tuning):
+
+
+```cpp
+sm.fromString("60:256\n256/243\n9/8\n32/27\n81/64\n4/3\n729/512\n3/2\n128/81\n27/16\n16/9\n243/128\n2");
+
+for (int note=60; note<72; ++note)
+  cout << note << " : " << sm.noteToFreq(note) << "\n";
+```
+```console
+60 : 256
+61 : 269.695
+62 : 288
+63 : 303.407
+64 : 324
+65 : 341.333
+66 : 364.5
+67 : 384
+68 : 404.543
+69 : 432
+70 : 455.111
+71 : 486
+
+```
+
+`fromString` is *thread-safe*. Simultaneously calling `noteToFreq` in the audio thread and `fromString` in the GUI thread will not cause data races.
--- scalemap.cpp
+++ scalemap.cpp
@@ -1,40 +1,86 @@
-struct ScaleMap
-{
-  std::vector<double> scale;
-  int baseNote;
-  double baseFreq;
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <sstream>
+#include <deque>
 
-  double midiToFreq(int note)
+#include "tinyexpr.h"
+
+using namespace std;
+
+struct ScaleMap {
+  atomic<int> baseNote;
+  atomic<double> baseFreq;
+  atomic<int> scaleSize;
+  deque< atomic<double> > scale;
+
+  ScaleMap(string str = "60:440*2^-0.75\n2^(1/12)\n2^(1/6)\n2^(1/4)\n2^(1/3)\n2^(5/12)\n2^(1/2)\n2^(7/12)\n2^(2/3)\n2^(3/4)\n2^(5/6)\n2^(11/12)\n2",
+           int maxScaleSize=128)
   {
-    int degree = (note - baseNote) % scale.length;
-    if (degree<0) degree += scale.length;
-    return baseFreq * scale[degree] *
-           pow(max(scale), floor((note - baseNote - 1) / scale.length));
+    scale.resize(maxScaleSize);
+    fromString(str);
   }
 
-  void loadSCL(std::string scl)
-  {
-    int lineStart = 0;
-    for (int i=0; i<scl.length; ++i)
-    {
-      if (scl[lineStart]!="!")
-      {
-        // read
-      }
-      if (scl[i]=="\n") lineStart=i+1;
-    }
+  double noteToFreq(int note) {
+
+    // atomic access
+    int baseNoteNow = baseNote.load();
+    double baseFreqNow = baseFreq.load();
+    int scaleSizeNow = scaleSize.load();
+
+    if (scaleSizeNow <= 0) return baseFreqNow;
+
+    int degree = (note-baseNoteNow-1) % scaleSizeNow;
+    if (degree<0) degree += scaleSizeNow; // true modulo
+    return baseFreqNow *
+           scale[degree].load() *
+           pow(scale[scaleSizeNow-1],
+               (note-baseNoteNow-1-degree) / scaleSizeNow);
   }
 
-  void loadKBM(std::string kbm)
-  {
-    int lineStart = 0;
-    for (int i=0; i<scl.length; ++i)
+  int fromString(string str) {
+    int baseNoteTmp;
+    double baseFreqTmp;
+    int scaleSizeTmp = 0;
+
+    istringstream iss(str);
+    string line;
+    size_t colon;
+
+    if (getline(iss, line) &&
+        (colon = line.find(':')) > 0 &&
+        colon < line.length() - 1 &&
+        !isnan(baseFreqTmp = te_interp(&line[colon+1], 0))
+       )
     {
-      if (scl[lineStart]!="!")
+      try {baseNoteTmp = stoi(line);} catch (...) { return 1; }
+
+      baseNote.store(baseNoteTmp);
+      baseFreq.store(baseFreqTmp);
+
+      double freqRatio;
+      while (getline(iss, line) &&
+             scaleSizeTmp < scale.size() &&
+             !isnan(freqRatio = te_interp(&line[0], 0)))
       {
-        // read
+        scale[scaleSizeTmp].store(freqRatio);
+        scaleSizeTmp++;
       }
-      if (scl[i]=="\n") lineStart=i+1;
+
+      scaleSize.store(scaleSizeTmp);
+
+      return 0;
     }
+    else return 1;
   }
 };
+
+int main() {
+  ScaleMap sm;
+  sm.fromString("60:256\n256/243\n9/8\n32/27\n81/64\n4/3\n"
+                "729/512\n3/2\n128/81\n27/16\n16/9\n243/128\n2");
+  for (int note=60; note<73; ++note) {
+    cout << note << ' ';
+    cout << sm.noteToFreq(note) << '\n';
+  }
+}
